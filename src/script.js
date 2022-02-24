f71a6feafa62dc659d58b4cfe8aa856da9251fc9
Ashutoshm17121999 <ashutoshmishra2022jan@cedcoss.com>
2022-02-24 11:46:28 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,112 @@
+$(document).ready(function(){
+    console.log("loaded");
+    $(document).on('click','#addBtn',function(){
+console.log("skjdgfhjsg");
+        $.ajax({
+            'url':'todo.php',
+            'method':'POST',
+            'data':{'input':$('#new-task').val()},
+            //'datatype':'JSON'
+        }).done(function(data){
+            d=$.parseJSON(data);
+            console.log(d);
+            displayTodo(d);
+        })
+    })
+})
+$(document).ready(function(){
+    $('#incomplete-tasks').on('click','#delete',function(){
+        $.ajax({
+            'url':'todo.php',
+            'method':'POST',
+            'data':{'input-pos':$(this).data('index')},
+            'datatype':'JSON'
+        }).done(function(data){
+            d=$.parseJSON(data);
+            displayTodo(d);
+        })
+    })
+})
+$(document).ready(function(){
+    $(document).on('click','.edit',function(){
+        var index=$(this).data('ind');
+        // console.log("asasa");
+        $.ajax({
+            'url':'todo.php',
+            'method':'POST',
+            'data':{'ipos':$(this).data('ind')},
+            'datatype':'JSON'
+        }).done(function(data){
+            d=$.parseJSON(data);
+            // displayTodo(d);
+            text1 = "";
+            text1+='<input id="new-task" type="text"><button id="addBtn" name="addBtn">Add</button><button id="updateBtn" data-pid='+index+' name="updateBtn">Update</button>'
+            $('#para').html(text1);
+            $('#new-task').val(d[index]);
+            $('#addBtn').hide();
+            $('#updateBtn').show();
+            displayTodo(d);
+        })
+    })
+})
+$(document).ready(function(){
+    $(document).on('click','#updateBtn',function(){
+        $('#addBtn').show();
+        $('#updateBtn').hide();
+        $.ajax({
+            'url':'todo.php',
+            'method':'POST',
+            'data':{'inputUpdt':$('#new-task').val(),'myIndex':$(this).data('pid')},
+            'datatype':'JSON'
+    }).done(function(data){
+        d=$.parseJSON(data);
+            displayTodo(d);
+    });
+});
+});
+
+$(document).ready(function(){   
+    $('#incomplete-tasks').on('change','#check',function(){   
+        console.log(100);    
+        $.ajax({
+            'url':'todo.php',
+            'method':'POST',
+            'data':{'pos':$(this).data('check')},
+            'datatype':'JSON'
+    }).done(function(data){
+        console.log(data);
+        d=$.parseJSON(data);
+        console.log(d);
+        displayTodo(d['incomplete']);
+        console.log(d['complete']);
+        displayComplete(d['complete']);
+    });
+    });
+});
+$(document).ready(function(){
+    $('#completed-tasks').on('click','#delete1',function(){
+        $.ajax({
+            'url':'todo.php',
+            'method':'POST',
+            'data':{'input-posit':$(this).data('index1')},
+            'datatype':'JSON'
+        }).done(function(data){
+            d=$.parseJSON(data);
+            displayComplete(d);
+        })
+    })
+})
+function displayTodo(myData){
+    var text="";
+    for (i=0;i<myData.length;i++){
+        text += '<li><input type="checkbox" data-check='+i+' id="check" name="check"><label>'+myData[i]+'</label><input type="text"><button id="edit" data-ind='+i+' name="editBtn" class="edit">Edit</button><button id="delete" data-index='+i+' class="delete" name="deleteBtn">Delete</button></li>'
+    }
+    $('#incomplete-tasks').html(text);
+}
+function displayComplete(myData){
+    var text="";
+    for(i=0;i<myData.length;i++){
+        text += '<li><input type="checkbox" checked name="check"><label>'+myData[i]+'</label><input type="text"><button id="edit1" data-i='+i+' name="editBtn" class="edit">Edit</button><button id="delete1" data-index1='+i+' class="delete" name="deleteBtn">Delete</button></li><input type="text" hidden name="myVal" value="'+i+'">';
+    }
+    $('#completed-tasks').html(text);
+}
